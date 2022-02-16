eaee2ddb8a23982fc44f46a26067a5382bf37a94
reverie64 <kristinh889@gmail.com>
2022-02-16 17:57:43 -0600

    basic structure

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
+
+const container = document.getElementById('container');
+const book = document.getElementById('book');
+
+
+let mylibrary = [
+    "harry potter", "game of thrones", "noragami"
+];
+
+
 function Books(title, author, pages, complete){
     this.title = title
     this.author = author
@@ -8,3 +18,32 @@ function Books(title, author, pages, complete){
     }
     console.log(Books.info());
 }
+
+
+// displays all books
+function showBooks () {
+    for (let i = 0; i < mylibrary.length; i++){
+        // display each book
+    }
+
+} // or use for each?
+
+
+// allows user to input a new book in form (popup?)
+function addBook(){
+  
+}
+
+// button to remove book from library
+function remove(){
+
+}
+
+
+// toggles book to read or unread based on book prototype
+function readBook(){
+
+}
+
+
+mylibrary.push()
