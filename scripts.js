cffa523eb56a70305c6198a5d4289b9b0a66940c
reverie64 <kristinh889@gmail.com>
2022-02-21 12:09:14 -0600

    added dom elements, created form, display books.

--- scripts.js
+++ scripts.js
@@ -1,49 +1,67 @@
 
 const container = document.getElementById('container');
-const book = document.getElementById('book');
+const shelf = document.getElementById('bookshelf');
 
+const title1 = document.querySelector('#title');
+const author = document.querySelector('#author');
+const title = document.querySelector('#pages');
+const start = document.querySelector('#start');
+const end = document.querySelector('#end');
 
-let mylibrary = [
-    "harry potter", "game of thrones", "noragami"
-];
+const newBut = document.querySelector('#new');
+const form = document.querySelector('#form');
 
+const read = document.getElementsByClassName('switch');
 
-function Books(title, author, pages, complete){
-    this.title = title
+let myLibrary =  ['harry potter', 'noragami', 'fruits basket'];
+
+/*
+function Book(title, author, pages, complete, numOfBook){
+    this.title = title // display each book
     this.author = author
     this.pages = pages
     this.complete = complete
     this.info = function() {
        return title, author, pages, complete
     }
-    console.log(Books.info());
-}
+};
+//Object.create
+console.log(Book.info());
+*/
+
+// display each book   // or use for each
+function showBooks() {
+    for (let i = 0; i < myLibrary.length; i++){
+        const bookCard = document.createElement('div');
+        bookCard.classList.add('book');
+        bookCard.textContent = myLibrary[i];
+        shelf.appendChild(bookCard);
+    };
+};
+
+showBooks();
 
 
-// displays all books
-function showBooks () {
-    for (let i = 0; i < mylibrary.length; i++){
-        // display each book
-    }
 
-} // or use for each?
+// button allows user to input a new book through a popup form 
+function addBook() {
+  // click button,  form 
+
+};
+
+newBut.addEventListener('click', addBook)
 
 
-// allows user to input a new book in form (popup?)
-function addBook(){
-  
-}
 
 // button to remove book from library
-function remove(){
+function removeBook(){
 
 }
 
 
 // toggles book to read or unread based on book prototype
 function readBook(){
-
+ //   read
 }
 
 
-mylibrary.push()
